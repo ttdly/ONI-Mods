484b96230a020912e58aeb6141d3480c65721151
秃头灯笼鱼 <ttdlyu@163.com>
2023-07-02 11:40:54 +0800

    新修复：FluidShipping 罐装气体液体直接排出。

--- FixPack/String.cs
+++ FixPack/String.cs
@@ -7,6 +7,7 @@ namespace FixPack {
                 public class GENERAL {
                     public static LocString ACTIVE = (LocString)"Active";
                 }
+                
                 public class DESTRUCTIBLE_FEATURES {
                     public static LocString TITLE = (LocString)"Destructible Features";
                     public static LocString DESC = (LocString)"Destroy Natural Feature";
@@ -15,9 +16,18 @@ namespace FixPack {
                     public static LocString OPTION3 = (LocString)"Anaylsis time";
                     public static LocString OPTION4 = (LocString)"Deconstruct time";
                 }
+                
                 public class DECONSTRUCTABLE_PROPS {
                     public static LocString TITLE = (LocString)"Deconstructable POI Props";
                     public static LocString DESC = (LocString)"This mod allows the deconstruction of all POI props.";
+                }
+
+                public class FLUID_SHIPPING {
+                    public static LocString TITLE = (LocString)"FluidShipping";
+                    public static LocString DESC = (LocString)"Mopping and plumbing left you with more bottled and canistered fluids than you know what to do with? Never fear - fluid shipping is here!";
+                    public static LocString OPTION1 = (LocString)"Canister Inserter Volume";
+                    public static LocString OPTION2 = (LocString)"Bottle Inserter Volume";
+                    public static LocString OPTION3 = (LocString)"Bottle Filler Volume";
                 }
             }
         }
