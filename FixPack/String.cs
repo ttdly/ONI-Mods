669fc14686e94d8c92423de5489e03824cc65248
秃头灯笼鱼 <ttdlyu@163.com>
2023-07-17 12:38:09 +0800

    删除修复包，添加泉可移动

---
+++ FixPack/String.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace FixPack {
+    public class String {
+        public class UI {
+            public class FIX_PACK {
+                public class GENERAL {
+                    public static LocString ACTIVE = (LocString)"Active";
+                }
+                
+                public class DESTRUCTIBLE_FEATURES {
+                    public static LocString TITLE = (LocString)"Destructible Features";
+                    public static LocString DESC = (LocString)"Destroy Natural Feature";
+                    public static LocString OPTION1 = (LocString)"Remove neutronium";
+                    public static LocString OPTION2 = (LocString)"Replace neutronium with obsidian";
+                    public static LocString OPTION3 = (LocString)"Anaylsis time";
+                    public static LocString OPTION4 = (LocString)"Deconstruct time";
+                }
+                
+                public class DECONSTRUCTABLE_PROPS {
+                    public static LocString TITLE = (LocString)"Deconstructable POI Props";
+                    public static LocString DESC = (LocString)"This mod allows the deconstruction of all POI props.";
+                }
+
+                public class STORAGE_POD {
+                    public static LocString TITLE = (LocString)"Storage Pod";
+                    public static LocString DESC = (LocString)"";
+                    public static LocString OPTION1 = (LocString)"Pod Capacity";
+                    public static LocString OPTION2 = (LocString)"Cool Pod Capacity";
+                    public static LocString OPTION3 = (LocString)"Pod Stores Food";
+                }
+            }
+        }
+    }
+}
---
+++ MoveGeysers/Mod.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using PeterHan.PLib.AVC;
+using PeterHan.PLib.Core;
+using UnityEngine;
+
+namespace MoveGeysers {
+    public class Mod :KMod.UserMod2{
+        public override void OnLoad(Harmony harmony) {
+            base.OnLoad(harmony);
+            PUtil.InitLibrary();
+            new PVersionCheck().Register(this, new SteamVersionChecker());
+        }
+    }
+
+    [HarmonyPatch(typeof(GeyserGenericConfig), nameof(GeyserGenericConfig.CreateGeyser))]
+    public static class GeyserGenericConfig_CreateGeyser_Patch {
+        public static void Postfix(GameObject __result) {
+            Pickupable pickupable = __result.AddOrGet<Pickupable>();
+            pickupable.SetWorkTime(50f);
+            pickupable.sortOrder = 0;
+            __result.AddOrGet<Movable>();
+        }
+    }
+}
