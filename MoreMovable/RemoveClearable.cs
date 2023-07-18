0a6075bfecfe99e3f0436684f4284ee102cc9896
秃头灯笼鱼 <ttdlyu@163.com>
2023-07-18 17:31:34 +0800

    MoreMovable: 能移动更多的东西

---
+++ MoreMovable/RemoveClearable.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace MoreMovable {
+    public class RemoveClearable : KMonoBehaviour {
+        protected override void OnSpawn() {
+            base.OnSpawn();
+            Destroy(gameObject.GetComponent<Clearable>());
+        }
+    }
+}
