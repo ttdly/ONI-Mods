6879749c0cf0b425fba3b63bf6aad370cce5c821
秃头灯笼鱼 <ttdlyu@163.com>
2023-08-29 21:49:28 +0800

    ChangeChoreType：养殖供应 #17

--- ChangeChoreType/Mod.cs
+++ ChangeChoreType/Mod.cs
@@ -12,7 +12,7 @@ namespace ChangeChoreType {
 [HarmonyPatch(typeof(ChoreType), MethodType.Constructor, typeof(string), typeof(ResourceSet), typeof(string[]), typeof(string), typeof(string), typeof(string), typeof(string), typeof(IEnumerable<Tag>),typeof(int), typeof(int))]
 public class ChoreType_Patch {
     public static void Prefix(string id, ref string[] chore_groups) {
-        if (id == "FoodFetch" || id == "EquipmentFetch") {
+        if (id == "FoodFetch" || id == "EquipmentFetch" || id == "RanchingFetch") {
             chore_groups = new string[1] { "Storage" };
         }
     }
