e24b1ee36fad323cf07f0c4cf171c13d68101eb3
秃头灯笼鱼 <ttdlyu@163.com>
2023-12-04 09:42:31 +0800

    PackAnything 的修复模组

---
+++ FixCrashBMF/BeaconConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using TUNING;
+using UnityEngine;
+
+namespace FixCrashBMF {
+    public class BeaconConfig : IEntityConfig {
+        public const string ID = "Beacon";
+        public const float mass = 200f;
+        public GameObject CreatePrefab() {
+            string name = "NULL";
+            string desc = "NULL";
+            EffectorValues dector = BUILDINGS.DECOR.BONUS.TIER1;
+            EffectorValues noisy = NOISE_POLLUTION.NOISY.TIER6;
+            KAnimFile anim = Assets.GetAnim((HashedString)"displacement_beacon_kanim");
+            GameObject placeEntity = EntityTemplates.CreatePlacedEntity(ID, name, desc, mass, anim, "object", Grid.SceneLayer.Front, 1, 2, dector, noisy, additionalTags: new List<Tag>() { GameTags.IndustrialProduct });
+            Pickupable pickupable = placeEntity.AddOrGet<Pickupable>();
+            pickupable.SetWorkTime(5f);
+            pickupable.sortOrder = 0;
+            placeEntity.AddOrGet<Movable>();
+            placeEntity.AddOrGet<Beacon>();
+            placeEntity.AddOrGet<UserNameable>();
+            return placeEntity;
+        }
+
+        public string[] GetDlcIds() => DlcManager.AVAILABLE_ALL_VERSIONS;
+
+        public void OnPrefabInit(GameObject inst) {
+        }
+
+        public void OnSpawn(GameObject inst) {
+        }
+    }
+}
---
+++ FixCrashBMF/Mod.cs
@@ -0,0 +1,48 @@
+﻿using Database;
+using HarmonyLib;
+using PeterHan.PLib.AVC;
+using PeterHan.PLib.Core;
+using System;
+using System.Collections.Generic;
+using STRINGS;
+
+namespace PackAnything {
+    public class Mod : KMod.UserMod2 {
+        public override void OnLoad(Harmony harmony) {
+            base.OnLoad(harmony);
+            PUtil.InitLibrary();
+            new PVersionCheck().Register(this, new SteamVersionChecker());
+        }
+    }
+
+    public class StaticVars {
+        public static SkillPerk CanPack;
+        public static Skill Pack;
+        public static void Init() {
+            CanPack = Db.Get().SkillPerks.Add(new SimpleSkillPerk(nameof(CanPack), "NULL"));
+            Pack = Db.Get().Skills.Add(new Skill(nameof(Pack), "NULL", "NULL", "", 3, "hat_role_building3", "skillbadge_role_building3", Db.Get().SkillGroups.Building.Id, new List<SkillPerk> { Db.Get().SkillPerks.IncreaseConstructionLarge, CanPack }, new List<string> { Db.Get().Skills.Building3.Id }));
+        }
+    }
+
+    public class Patches {
+        [HarmonyPatch(typeof(MinionResume), "OnSpawn")]
+        public static class MinionResume_OnSpawn_Patch {
+            public static void Postfix(MinionResume __instance) {
+                if (__instance.HasPerk(StaticVars.CanPack)) {
+                    __instance.UnmasterSkill(StaticVars.Pack.Id);
+                    __instance.SetHats(__instance.CurrentHat, null);
+                    __instance.ApplyTargetHat();
+                    Notification notification = new Notification((string)MISC.NOTIFICATIONS.RESETSKILL.NAME, NotificationType.Good, (Func<List<Notification>, object, string>)((notificationList, data) => (string)MISC.NOTIFICATIONS.RESETSKILL.TOOLTIP + notificationList.ReduceMessages(false)));
+                    __instance.GetComponent<Notifier>().Add(notification);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+        public class GeneratedBuildings_LoadGeneratedBuildings_Patch {
+            public static void Prefix() {
+                StaticVars.Init();
+            }
+        }
+    }
+}
