1106e2f6972178f26df6a21241eb4d462695bfd9
秃头灯笼鱼 <ttdlyu@163.com>
2024-03-17 10:19:20 +0800

    SpaceStore: 简单写了一下商店的模块

---
+++ SpaceStore/Store/LinkToStore.cs
@@ -0,0 +1,65 @@
+﻿using KSerialization;
+using SpaceStore.StoreRoboPanel;
+using System;
+using TUNING;
+
+namespace SpaceStore.Store {
+    public class LinkToStore: Workable{
+        [Serialize]
+        public bool isLinked = false;
+        [Serialize] 
+        public bool markToLink = false;
+        [MyCmpReq]
+        public Storage storage;
+
+        public Chore chore;
+        private static readonly EventSystem.IntraObjectHandler<LinkToStore> OnRefreshUserMenuDelegate = new EventSystem.IntraObjectHandler<LinkToStore>((component, data) => component.OnRefreshUserMenu(data));
+        private static readonly EventSystem.IntraObjectHandler<LinkToStore> OnStorageChangeDelegate = new EventSystem.IntraObjectHandler<LinkToStore>((component, data) => component.OnStorageChange(data));
+
+        private void OnStorageChange(object _) {
+            if (!isLinked) return;
+            if ((storage.MassStored() / storage.capacityKg) == 1) {
+                storage.ConsumeAllIgnoringDisease();
+            } 
+        }
+
+        private void OnRefreshUserMenu(object _) {
+            if (isLinked || markToLink) return;
+            Game.Instance.userMenu.AddButton(gameObject, new KIconButtonMenu.ButtonInfo("action_repair", MyString.UI.ROBO_PANEL.NAME, new System.Action(new System.Action(() => { markToLink = true; UpdateChore(); }))));
+        }
+
+        private void UpdateChore() {
+            if (chore == null && markToLink) {
+                chore = new WorkChore<LinkToStore>(Db.Get().ChoreTypes.Build, this, null, run_until_complete: true, null, null, null, allow_in_red_alert: true, null, ignore_schedule_block: false, only_when_operational: false);
+            } else if (chore != null) {
+                chore.Cancel("no longer need");
+                chore = null;
+            }
+        }
+
+        protected override void OnCompleteWork(Worker worker) {
+            base.OnCompleteWork(worker);
+            isLinked = true;
+        }
+
+        protected override void OnSpawn() {
+            base.OnSpawn();
+            workerStatusItem = Db.Get().DuplicantStatusItems.Building;
+            workingStatusItem = null;
+            attributeConverter = Db.Get().AttributeConverters.ConstructionSpeed;
+            attributeExperienceMultiplier = DUPLICANTSTATS.ATTRIBUTE_LEVELING.MOST_DAY_EXPERIENCE;
+            minimumAttributeMultiplier = 0.75f;
+            skillExperienceSkillGroup = Db.Get().SkillGroups.Building.Id;
+            skillExperienceMultiplier = SKILLS.MOST_DAY_EXPERIENCE;
+            Prioritizable.AddRef(base.gameObject);
+            synchronizeAnims = false;
+            multitoolContext = "build";
+            multitoolHitEffectTag = EffectConfigs.BuildSplashId;
+            workingPstComplete = null;
+            workingPstFailed = null;
+            Subscribe((int)GameHashes.RefreshUserMenu, OnRefreshUserMenuDelegate);
+            Subscribe((int)GameHashes.OnStorageChange, OnStorageChangeDelegate);
+            UpdateChore();
+        }
+    }
+}
---
+++ SpaceStore/Store/StoreList.cs
@@ -0,0 +1,96 @@
+﻿using SpaceStore.MyGeyser;
+using SpaceStore.StoreRoboPanel;
+using STRINGS;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceStore.Store
+{
+    public class StoreList
+    {
+
+        public static List<MarketItem> marketItems = new List<MarketItem>();
+
+        public enum ObjectType
+        {
+            Object,
+            Element,
+            NeedPack,
+        }
+
+        public class MarketItem
+        {
+            public Sprite sprite;
+            public string name = "test";
+            public int price;
+            public Tag tag;
+            public Tag innerTag;
+            public SimHashes simHashes;
+            public ObjectType type;
+            public int unit = 0;
+            public int count = 0;
+
+            // 初始化元素商品
+            public MarketItem
+                (Tag tag, SimHashes simHashes = 0, int unit = 0, int price = 10, ObjectType type = ObjectType.Element)
+            {
+                this.tag = tag;
+                this.price = price;
+                this.type = type;
+                this.simHashes = simHashes;
+                this.unit = unit;
+                sprite = Def.GetUISprite(tag).first;
+                name = UI.StripLinkFormatting(Strings.Get("STRINGS.ELEMENTS." + tag.ToString().ToUpper() + ".NAME"));
+            }
+
+
+            public MarketItem(Tag tag, int price = 10, ObjectType type = ObjectType.Object)
+            {
+                this.tag = tag;
+                sprite = Def.GetUISprite(tag).first;
+                this.price = price;
+                this.type = type;
+            }
+
+            public override string ToString()
+            {
+                return $"\n名称：\t{name}\n标签：\t{tag}\n价格：\t{price}\n数量：\t{count}\n单位：\t{unit}\n类型：\t{type}\n内部：\t{innerTag}";
+            }
+        }
+
+        public static List<Tuple<SimHashes, int, int>> elements = new List<Tuple<SimHashes, int, int>>() {
+            new Tuple<SimHashes, int, int>(SimHashes.Steel, 10, 500),
+            new Tuple<SimHashes, int, int>(SimHashes.SuperInsulator, 2, 300),
+            new Tuple<SimHashes, int, int>(SimHashes.SuperCoolant, 1, 200),
+        };
+
+        public static List<Tuple<string, int>> objects = new List<Tuple<string, int>>() {
+            new Tuple<string, int>(GeneShufflerRechargeConfig.ID, 400),
+            new Tuple<string, int>(RoboPanelConfig.ID,600),
+            new Tuple<string, int>(GeoActivatorConfig.ID, 600),
+        };
+
+        public static List<Tuple<string, int>> packs = new List<Tuple<string, int>>() {
+            new Tuple<string, int>(OilWellConfig.ID, 500),
+        };
+
+        public static void Init()
+        {
+            foreach (Tuple<SimHashes, int, int> element in elements)
+            {
+                marketItems.Add(new MarketItem(element.first.CreateTag(), element.first, element.second, element.third));
+            }
+
+
+            foreach (Tuple<string, int> obj in objects)
+            {
+                marketItems.Add(new MarketItem(obj.first, obj.second));
+            }
+
+            //foreach (Tuple<string, int> pack in packs)
+            //{
+            //    marketItems.Add(new MarketItem(Assets.GetPrefab(pack.first).GetProperName(), pack.first, pack.second));
+            //}
+        }
+    }
+}
