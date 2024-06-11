e8ab131cb386820ab373e5fe8e544aa8de947f6b
秃头灯笼鱼 <ttdlyu@163.com>
2024-06-11 18:54:30 +0800

    PackAnything: 字符串修正；俄语翻译 #31

--- PackAnything/ModString.cs
+++ PackAnything/ModString.cs
@@ -2,7 +2,7 @@
   public class ModString {
     public class Options {
       public static LocString GENERATE_UNOBTANIUM = "Generate Unobtanium";
-      public static LocString GENERATE_UNOBTANIUM_DESC = "Whether or not does moving a geyser create Unobtanium";
+      public static LocString GENERATE_UNOBTANIUM_DESC = "Whether or not does moving a geyser create Unobtanium.";
       public static LocString TOGGLE_GEYSER_NUM = "Toggle Geyser Num";
       public static LocString TOGGLE_GEYSER_NUM_DESC = "When moving a geyser, do not change its attributes.";
     }
