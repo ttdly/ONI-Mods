5d5c003047709a20732a4b3c6567c16ee58156a1
秃头灯笼鱼 <ttdlyu@163.com>
2024-08-27 21:34:37 +0800

    PackAnything: 添加复制人移动

---
+++ PackAnything/Movable/CreatureMovable.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace PackAnything.Movable {
+  public class CreatureMovable : BaseMovable{
+    public override void Move(int targetCell) {
+      base.StableMove(targetCell);
+    }
+    
+    #region 补丁
+
+    [HarmonyPatch(typeof(EntityTemplates), "CreateAndRegisterBaggedCreature")]
+    public class Patch_1 {
+      private static void Postfix(GameObject creature) {
+        Destroy(creature.GetComponent<global::Movable>());
+        creature.AddOrGet<CreatureMovable>();
+      }
+    }
+    
+    [HarmonyPatch(typeof(MinionConfig), "CreatePrefab")]
+    public class Patch_2 {
+      private static void Postfix(GameObject __result) {
+        Destroy(__result.GetComponent<global::Movable>());
+        __result.AddOrGet<CreatureMovable>();
+      }
+    }
+    #endregion
+  }
+}
