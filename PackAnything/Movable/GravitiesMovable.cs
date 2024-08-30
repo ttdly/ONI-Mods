dc0a58910c74db18d6aa494c46d7300d71ba33e2
秃头灯笼鱼 <ttdlyu@163.com>
2024-08-30 10:45:31 +0800

    PackAnything: 修复模组会使房间概览翻译失效的问题

--- PackAnything/Movable/GravitiesMovable.cs
+++ PackAnything/Movable/GravitiesMovable.cs
@@ -1,10 +1,12 @@
-﻿using HarmonyLib;
+﻿using System.Reflection;
+using HarmonyLib;
 using UnityEngine;
 using static PackAnything.Movable.StaticMethods;
 
 namespace PackAnything.Movable {
   public class GravitiesMovable : BaseMovable {
     public bool needNew = true;
+
     public override void Move(int targetCell) {
       base.Move(targetCell);
       var template = needNew ? Assets.GetPrefab(gameObject.PrefabID()) : gameObject;
@@ -18,27 +20,41 @@ namespace PackAnything.Movable {
 
     #region 补丁
 
+    public static void PatchBuildings(Harmony harmony) {
+      var targetMethod_common_1 = typeof(TemporalTearOpenerConfig).GetMethod("DoPostConfigureComplete");
+      var targetMethod_noCrossMove_1 = typeof(WarpConduitSenderConfig).GetMethod("DoPostConfigureComplete");
+      var targetMethod_noCrossMove_2 = typeof(WarpConduitReceiverConfig).GetMethod("DoPostConfigureComplete");
+      var targetMethod_headquarters = typeof(HeadquartersConfig).GetMethod("DoPostConfigureComplete");
+      var postfix_common = AccessTools.Method(typeof(GravitiesMovable), nameof(PostfixCommon)); 
+      var postfix_noCrossMove = AccessTools.Method(typeof(GravitiesMovable), nameof(PostfixNoCrossMove));
+      var postfix_headquarters = AccessTools.Method(typeof(GravitiesMovable), nameof(PostfixHeadquarters));
+      harmony.Patch(targetMethod_common_1, postfix: new HarmonyMethod(postfix_common));
+      harmony.Patch(targetMethod_noCrossMove_1, postfix: new HarmonyMethod(postfix_noCrossMove));
+      harmony.Patch(targetMethod_noCrossMove_2, postfix: new HarmonyMethod(postfix_noCrossMove));
+      harmony.Patch(targetMethod_headquarters, postfix: new HarmonyMethod(postfix_headquarters));
+    }
+
+    public static void PostfixNoCrossMove(GameObject go) {
+      go.AddOrGet<GravitiesMovable>().canCrossMove = false;
+    }
+
+    public static void PostfixHeadquarters(GameObject go) {
+      var movable = go.AddOrGet<GravitiesMovable>();
+      movable.canCrossMove = false;
+      movable.needNew = false;
+    }
+
+    public static void PostfixCommon(GameObject go) {
+      go.AddOrGet<GravitiesMovable>();
+    }
+
     [HarmonyPatch(typeof(GeneShufflerConfig), nameof(GeneShufflerConfig.CreatePrefab))]
     public class Patch_1 {
       public static void Postfix(GameObject __result) {
         __result.AddOrGet<GravitiesMovable>();
       }
     }
-
-    [HarmonyPatch(typeof(TemporalTearOpenerConfig), nameof(TemporalTearOpenerConfig.DoPostConfigureComplete))]
-    public class Patch_2 {
-      public static void Postfix(GameObject go) {
-        go.AddOrGet<GravitiesMovable>();
-      }
-    }
-
-    [HarmonyPatch(typeof(WarpConduitSenderConfig), nameof(WarpConduitSenderConfig.DoPostConfigureComplete))]
-    public class Patch_3 {
-      public static void Postfix(GameObject go) {
-        go.AddOrGet<GravitiesMovable>().canCrossMove = false;
-      }
-    }
-
+    
     [HarmonyPatch(typeof(WarpConduitReceiverConfig), nameof(WarpConduitReceiverConfig.DoPostConfigureComplete))]
     public class Patch_4 {
       public static void Postfix(GameObject go) {
